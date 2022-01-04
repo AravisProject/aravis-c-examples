2145185562ec866b8511d2aa51c5eafb659c1221
Emmanuel Pacaud <emmanuel.pacaud@lapp.in2p3.fr>
2022-01-04 16:25:26 +0100

    singleacquisition: add more comments and remove g_auto

--- 01-single-acquisition.c
+++ 01-single-acquisition.c
@@ -1,29 +1,45 @@
+/* SPDX-License-Identifier:Unlicense */
+
+/* Aravis header */
+
 #include <arv.h>
+
+/* Standard headers */
+
 #include <stdlib.h>
 #include <stdio.h>
 
 int
 main (int argc, char **argv)
 {
-	g_autoptr (ArvCamera) camera = NULL;
-	g_autoptr (GError) error = NULL;
-	g_autoptr (ArvBuffer) buffer = NULL;
+	ArvCamera *camera;
+	ArvBuffer *buffer;
+	GError *error = NULL;
 
+	/* Connect to the first available camera */
 	camera = arv_camera_new (NULL, &error);
 
 	if (ARV_IS_CAMERA (camera)) {
 		printf ("Found camera '%s'\n", arv_camera_get_model_name (camera, NULL));
 
+		/* Acquire a single buffer */
 		buffer = arv_camera_acquisition (camera, 0, &error);
 
 		if (ARV_IS_BUFFER (buffer)) {
+			/* Display some informations about the retrieved buffer */
 			printf ("Acquired %d×%d buffer\n",
 				arv_buffer_get_image_width (buffer),
 				arv_buffer_get_image_height (buffer));
+			/* Destroy the buffer */
+			g_clear_object (&buffer);
 		}
+
+		/* Destroy the camera instance */
+		g_clear_object (&camera);
 	}
 
 	if (error != NULL) {
+		/* En error happened, display the correspdonding message */
 		printf ("Error: %s\n", error->message);
 		return EXIT_FAILURE;
 	}
