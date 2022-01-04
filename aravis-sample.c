b88c24158568e36b7e942d2a0fe85a11e0bfa8c1
Emmanuel Pacaud <emmanuel.pacaud@lapp.in2p3.fr>
2022-01-04 15:51:11 +0100

    Code import

---
+++ aravis-sample.c
@@ -0,0 +1,32 @@
+#include <arv.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int
+main (int argc, char **argv)
+{
+	g_autoptr (ArvCamera) camera = NULL;
+	g_autoptr (GError) error = NULL;
+	g_autoptr (ArvBuffer) buffer = NULL;
+
+	camera = arv_camera_new (NULL, &error);
+
+	if (ARV_IS_CAMERA (camera)) {
+		printf ("Found camera '%s'\n", arv_camera_get_model_name (camera, NULL));
+
+		buffer = arv_camera_acquisition (camera, 1000, &error);
+
+		if (ARV_IS_BUFFER (buffer)) {
+			printf ("Acquired %d×%d buffer\n",
+				arv_buffer_get_image_width (buffer),
+				arv_buffer_get_image_height (buffer));
+		}
+	}
+
+	if (error != NULL) {
+		printf ("Error: %s\n", error->message);
+		return EXIT_FAILURE;
+	}
+
+	return EXIT_SUCCESS;
+}
